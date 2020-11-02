44abcd40d96309a5b0571eb2850bb7b6405d13fa
chenshuyang <3058721813@qq.com>
2020-11-02 10:45:51 +0800

    【+】【-】替换http调用工具类

---
+++ demo/src/http/HttpClient.java
@@ -0,0 +1,69 @@
+package http;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class HttpClient {
+    public static String doGet(String httpurl) {
+        String result = null;
+        try {
+            URL url = new URL(httpurl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(15000);
+            connection.setReadTimeout(60000);
+            connection.connect();
+            if (connection.getResponseCode() == 200) {
+                try (InputStream is = connection.getInputStream();
+                     BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+                    StringBuilder sbf = new StringBuilder();
+                    String temp;
+                    while ((temp = br.readLine()) != null) {
+                        sbf.append(temp);
+                        sbf.append("\r\n");
+                    }
+                    result = sbf.toString();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String doPost(String httpUrl, String param) {
+        String result = null;
+        try {
+            URL url = new URL(httpUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(15000);
+            connection.setReadTimeout(60000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            connection.setRequestProperty("Content-Type", "multipart/form-date;charset=utf-8");
+            connection.connect();
+            OutputStream os = connection.getOutputStream();
+            os.write(param.getBytes(StandardCharsets.UTF_8));
+            os.flush();
+            os.close();
+            InputStream is = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            if (connection.getResponseCode() == 200) {
+                StringBuilder sbf = new StringBuilder();
+                String temp;
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
